5a116bbfb8eb7568a400a9aba13ba76e378294de
richardl2003 <richardliy03@gmail.com>
2024-03-26 22:36:15 -0400

    feat: rescaffold of app

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,13 +1,46 @@
-import { Stack, Slot } from "expo-router";
-import { useSelector, useDispatch } from "react-redux";
-import { SessionProvider, useSession } from "../utils/ctx";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 
-const RootLayout = () => {
-    return (
-        <SessionProvider>
-            <Slot />
-        </SessionProvider>
-    )
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
 }
 
-export default RootLayout;
+function RootLayoutNav() {
+
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
---
+++ frontend/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const Page = () => {
+    return (
+        <View>
+            <Text>First Page</Text>
+        </View>
+    )
+}
+
+export default Page;
