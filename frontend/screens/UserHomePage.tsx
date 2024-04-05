dc8f84e9134020c80d219528a1899a316ddb2cd5
cpet02 <113526207+cpet02@users.noreply.github.com>
2024-04-05 11:26:24 -0400

    navbar for Userhomepage

--- frontend/screens/UserHomePage.tsx
+++ frontend/screens/UserHomePage.tsx
@@ -4,6 +4,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { ChatList, Summary, Profile } from '../components/';
 import { useStore } from '../utils/store';
 import { useEffect } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import React = require('react');
 
 const Tab = createBottomTabNavigator();
 
@@ -20,13 +22,26 @@ const UserHomePage = () => {
     })
 
     return (
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name="Chat" component={ChatList} />
-                <Tab.Screen name="Summary" component={Summary} />
-                <Tab.Screen name="Profile" component={Profile} />
-            </Tab.Navigator>
-        </NavigationContainer>
+    <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                  let iconName;
+                  if (route.name ==='Chat') {
+                      iconName = focused ? 'chat-bubble' : 'chat-bubble-outline' ;
+                  } else if (route.name === 'Notifications') {
+                      iconName = focused ? 'list' : 'list-outline' ;
+                  } else if (route.name === 'Profile') {
+                      iconName = focused ? 'person' : 'person-outline' ;
+                  }
+                  return <Ionicons name={iconName} size={size} color={color} />;
+              },
+          })}>
+            <Tab.Screen name="Chat" component={ChatList} />
+            <Tab.Screen name="Summary" component={Summary} />
+            <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
+    </NavigationContainer>
 
     );
 };
