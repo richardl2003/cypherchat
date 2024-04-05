67bc1dabf658542493f2386dddc45e068e984f0a
Michael Timmer <michaeltimmer19@gmail.com>
2024-04-04 23:16:45 -0400

    Setup channels and websocket consumers

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -9,8 +9,15 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django_channels_jwt_auth_middleware.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app
+})
